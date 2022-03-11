8da535bdb2638cfb3a7825828bef2e5b2a5108eb
Juan Soriano <juanky@novoda.com>
2022-03-10 22:48:49 -0600

    Create a Result type

---
+++ flutter/news/lib/core/result.dart
@@ -0,0 +1,40 @@
+import 'package:equatable/equatable.dart';
+
+abstract class Failure implements Exception {
+  final String message;
+  const Failure(this.message);
+}
+
+class Success<S> extends Equatable {
+  final S data;
+  const Success({required this.data});
+
+  @override
+  List<S> get props => [data];
+}
+
+class Result<S> extends Equatable {
+  final Failure? _failure;
+  final S? _data;
+
+  const Result(this._data, this._failure);
+
+  factory Result.failure(Failure failure) => Result(null, failure);
+  factory Result.success(S data) => Result(data, null);
+
+  Failure get failure {
+    assert(_failure != null);
+    return _failure!;
+  }
+
+  S get data {
+    assert(_data != null);
+    return _data!;
+  }
+
+  bool get isSuccess => _failure == null;
+  bool get isFailure => _failure != null;
+
+  @override
+  List<Object?> get props => [_failure, _data];
+}
