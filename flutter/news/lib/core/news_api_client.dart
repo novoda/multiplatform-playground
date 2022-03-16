0100c62991e4405c522a211a8ee2172924777881
Juan Soriano <juanky@novoda.com>
2022-03-16 12:22:10 -0600

    Remove unused import

--- flutter/news/lib/core/news_api_client.dart
+++ flutter/news/lib/core/news_api_client.dart
@@ -1,5 +1,4 @@
 import 'package:dio/dio.dart';
-import 'package:news/features/frontpage/domain/entities/article.dart';
 import 'package:news/features/frontpage/domain/entities/base_news_response.dart';
 import 'package:retrofit/retrofit.dart';
 part 'news_api_client.g.dart';
