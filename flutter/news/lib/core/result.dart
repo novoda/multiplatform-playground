a63d2556840d9bdc1c85809a898d6ee60d7a5ebd
Juan Soriano <juanky@novoda.com>
2022-03-15 22:55:59 -0600

    Makes different entities json serializable using json_serializable

--- flutter/news/lib/core/result.dart
+++ flutter/news/lib/core/result.dart
@@ -1,8 +1,11 @@
 import 'package:equatable/equatable.dart';
 
-abstract class Failure implements Exception {
+abstract class Failure extends Equatable implements Exception {
   final String message;
   const Failure(this.message);
+
+  @override
+  List<Object> get props => [message];
 }
 
 class Success<S> extends Equatable {
--- flutter/news/lib/features/frontpage/domain/entities/article.dart
+++ flutter/news/lib/features/frontpage/domain/entities/article.dart
@@ -1,16 +1,20 @@
+import 'package:equatable/equatable.dart';
+import 'package:json_annotation/json_annotation.dart';
 import 'package:news/features/frontpage/domain/entities/source.dart';
+part 'article.g.dart';
 
-class Article {
-  final Source? source;
-  final String? author;
-  final String? title;
-  final String? description;
-  final String? url;
-  final String? urlToImage;
-  final String? publishedAt;
-  final String? content;
+@JsonSerializable()
+class Article extends Equatable {
+  final Source source;
+  final String author;
+  final String title;
+  final String description;
+  final String url;
+  final String urlToImage;
+  final String publishedAt;
+  final String content;
 
-  Article(
+  const Article(
       {required this.source,
       required this.author,
       required this.title,
@@ -19,4 +23,20 @@ class Article {
       required this.urlToImage,
       required this.publishedAt,
       required this.content});
+
+  factory Article.fromJson(Map<String, dynamic> json) =>
+      _$ArticleFromJson(json);
+  Map<String, dynamic> toJson() => _$ArticleToJson(this);
+
+  @override
+  List<Object> get props => [
+        source,
+        author,
+        title,
+        description,
+        url,
+        urlToImage,
+        publishedAt,
+        content
+      ];
 }
