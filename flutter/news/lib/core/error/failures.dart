f0a6f744f719576e4f6057ccd93191d455bc448f
Juan Soriano <juanky@novoda.com>
2022-03-28 15:07:54 -0600

    Implements message on `InternalFailure`

--- flutter/news/lib/core/error/failures.dart
+++ flutter/news/lib/core/error/failures.dart
@@ -3,15 +3,17 @@ import 'package:freezed_annotation/freezed_annotation.dart';
 part 'failures.freezed.dart';
 
 abstract class InternalFailure {
-  final String message = "";
+  final String message;
+
+  InternalFailure(this.message);
 }
 
 @freezed
-class ServerFailure with _$ServerFailure implements InternalFailure {
+abstract class ServerFailure extends InternalFailure with _$ServerFailure {
   const factory ServerFailure({required String message}) = _ServerFailure;
 }
 
 @freezed
-class CacheFailure with _$CacheFailure implements InternalFailure {
+abstract class CacheFailure extends InternalFailure with _$CacheFailure {
   const factory CacheFailure({required String message}) = _CacheFailure;
 }
