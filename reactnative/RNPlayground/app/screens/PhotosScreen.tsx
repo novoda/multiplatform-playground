6442d3d021f13b7ad0a5b6c127d4ba81edc17640
Jozef Celuch <jozef@novoda.com>
2023-03-23 12:26:34 +0100

    Move conditional logic inside main component

--- reactnative/RNPlayground/app/screens/PhotosScreen.tsx
+++ reactnative/RNPlayground/app/screens/PhotosScreen.tsx
@@ -5,7 +5,7 @@ import { FlatList, Image, ScrollView, View } from "react-native"
 import { Button, Text } from "../components"
 import { colors, spacing } from "../theme"
 import { useStores } from "../models"
-import { Content, ErrorState, PhotosScreenUiState } from "../models/PhotosScreenStore"
+import { Content, ErrorState } from "../models/PhotosScreenStore"
 import { Card } from "react-native-paper"
 import { Photo } from "../models/Photo"
 
@@ -18,45 +18,33 @@ export const PhotosScreen: React.FC<PlaygroundScreenProps> = observer(() => {
   React.useEffect(() => {
     store.load()
   }, [store])
+
+  const { fullScreenLoading, error, content } = store.uiState
+  let renderedContent
+  if (fullScreenLoading) {
+    renderedContent = <LoadingComponent />
+  } else if (error) {
+    renderedContent = <ErrorComponent
+      error={error}
+      onResetClicked={store.load} />
+  } else if (content) {
+    renderedContent = <ContentComponent
+      content={content}
+      isLoading={store.uiState.isLoading}
+      loadNext={store.nextPage}
+    />
+  } else {
+    throw new Error(`UiState type is not handled ${JSON.stringify(store.uiState)}`)
+  }
   return (
     <View style={{
       backgroundColor: colors.background,
       flex: 1,
     }}>
-      {
-        StateComponent(
-          store.uiState,
-          store.load,
-          store.nextPage,
-        )
-      }
-
+      {renderedContent}
     </View>
   )
 })
-
-function StateComponent(
-  uiState: PhotosScreenUiState,
-  onResetClicked: () => {},
-  loadNext: () => {},
-) {
-  const { fullScreenLoading, error, content } = uiState
-
-  if (fullScreenLoading) {
-    return <LoadingComponent />
-  } else if (error) {
-    return <ErrorComponent
-      error={error}
-      onResetClicked={onResetClicked} />
-  } else if (content) {
-    return <ContentComponent
-      content={content}
-      isLoading={uiState.isLoading}
-      loadNext={loadNext}
-    />
-  }
-  throw new Error(`UiState type is not handled ${JSON.stringify(uiState)}`)
-}
 
 type ContentComponentProps = {
   content: Content
