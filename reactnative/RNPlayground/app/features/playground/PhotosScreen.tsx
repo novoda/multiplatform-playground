87f553ca5a6206b7ffb90c5341fed8cd140c6684
Jozef Celuch <jozef@novoda.com>
2023-04-12 12:42:12 +0200

    Add index and description to photos

--- reactnative/RNPlayground/app/features/playground/PhotosScreen.tsx
+++ reactnative/RNPlayground/app/features/playground/PhotosScreen.tsx
@@ -2,7 +2,7 @@ import * as React from "react"
 import { useAppDispatch, useAppSelector } from "../../hooks"
 import { PlaygroundTabScreenProps } from "../../navigators"
 import { FlatList, Image, ScrollView, View } from "react-native"
-import { spacing } from "../../theme"
+import { spacing, useAppTheme } from "../../theme"
 import { ActivityIndicator, Button, Card, Text } from "react-native-paper"
 import { Photo } from "./Photo"
 import { clear, fullScreenLoading, load, photosState, ReduxContent, ReduxError } from "./photoSlice"
@@ -52,38 +52,73 @@ type ContentComponentProps = {
   loadNext: () => void
 }
 
-type ItemProps = { photo: Photo }
-const Item = ({ photo }: ItemProps) => {
+type ItemProps = {
+  photo: Photo,
+  index: number
+}
+const Item = ({ photo, index }: ItemProps) => {
+  const colors = useAppTheme().colors
   return <Card
     style={
       {
         backgroundColor: photo.color,
         marginVertical: spacing.extraSmall,
         marginHorizontal: spacing.medium,
+        overflow: "hidden",
       }
     }
     elevation={0}
+    contentStyle={
+      {
+        height: 300,
+        borderRadius: 16,
+      }
+    }
   >
     <Image
       style={
         {
-          width: null,
-          flex: 1,
-          height: 300,
-          borderRadius: 16,
+          width: "100%",
+          height: "100%",
         }
       }
       source={{
         uri: photo.urls.regular,
       }}
     />
+    <View
+      style={{
+        width: "100%",
+        position: "absolute",
+        bottom: 0,
+        backgroundColor: colors.backdrop,
+        padding: spacing.small,
+      }}>
+      <Text
+        style={{
+          color: colors.onPrimary,
+          opacity: 0.8,
+        }}
+        variant={"bodyMedium"}>{index + 1}</Text>
+      {photo.description
+        ?
+        < Text
+          style={{
+            color: colors.onPrimary,
+            opacity: 0.8,
+          }}
+          variant={"bodyMedium"}>{photo.description}</Text>
+        : null
+      }
+    </View>
   </Card>
 }
 
 const ContentComponent = (props: ContentComponentProps) => (
   <FlatList
+    testID={"photos_content"}
     data={props.content.photos}
-    renderItem={({ item }) => <Item photo={item} />}
+    renderItem={({ item, index }) => <Item photo={item} index={index} />}
     keyExtractor={item => item.localId}
     onEndReachedThreshold={0.1}
     onEndReached={props.loadNext}
@@ -119,6 +154,6 @@ const ErrorComponent = ({ error, onResetClicked }: ErrorComponentProps) => (
 
 const LoadingComponent = () => (
   <View style={[{ justifyContent: "space-around", flexGrow: 1 }]}>
-    <ActivityIndicator animating={true} size={"large"} />
+    <ActivityIndicator animating={true} size={"large"} testID={"loading_indicator"} />
   </View>
 )
